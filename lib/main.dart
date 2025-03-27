be9625aa0fed32f181298475b5afefcd569dfca9
litauven <litauvensteven@gmail.com>
2025-03-27 14:19:39 +0700

    Login_Regis

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:explore_id/pages/Splash_Screen.dart';
 import 'package:explore_id/pages/home.dart';
+import 'package:explore_id/pages/Welcomepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: "Explore ID",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: SplashScreen(),
+      home: WelcomePage(),
     );
   }
 }
